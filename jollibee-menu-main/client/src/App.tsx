4edb71c1b602d1221eb4dfcbd427852d029e20a8
Alahuakbar <mhersanjohnsalenga@gmail.com>
2026-04-19 17:02:26 +0800

    12312

---
+++ jollibee-menu-main/client/src/App.tsx
@@ -0,0 +1,21 @@
+import { Router, Route } from "wouter";
+import { ErrorBoundary } from "@/components/ErrorBoundary";
+import Menu from "@/pages/Menu";
+import Cart from "@/pages/Cart";
+import NotFound from "@/pages/NotFound";
+import { CartProvider } from "@/contexts/CartContext";
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <CartProvider>
+        <Router>
+          <Route path="/" component={Menu} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/cart" component={Cart} />
+          <Route component={NotFound} />
+        </Router>
+      </CartProvider>
+    </ErrorBoundary>
+  );
+}
